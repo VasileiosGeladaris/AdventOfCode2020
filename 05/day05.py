326dffb9d18b1c6e70c188a98dd500672a2705ef
Vasileios Geladaris <vasilios.gel@gmail.com>
2020-12-05 22:29:15 +0200

    Solved 5-1, 5-2

---
+++ 05/day05.py
@@ -0,0 +1,41 @@
+with open('input') as file:
+    lines = file.readlines()
+    lines = [line[:len(line)-1] for line in lines]
+
+# PART 1:
+def readBPass(less, more, arr, path):
+    if len(arr) == 1:
+        return arr[0]
+    letter = path[0]
+    if letter == less:
+        return readBPass(less, more, arr[:int(len(arr)/2)], path[1:])
+    elif letter == more:
+        return readBPass(less, more, arr[int(len(arr)/2):], path[1:])
+
+max = -1
+for line in lines:
+    id = readBPass('F', 'B', range(128), line[:7]) * 8 + readBPass('L', 'R', range(8), line[7:])
+    if id > max:
+        max = id
+
+print(max)
+
+# PART 2:
+from itertools import product
+
+# Get all possible combinations for a passport.
+combs = [''.join(comb1) + ''.join(comb2) for comb1 in product(['F', 'B'], repeat=7) for comb2 in product(['L', 'R'], repeat=3)]
+
+# Get all the missing passports from the input.
+missing = [x for x in combs if x not in lines]
+
+# Get the row path for each missing passport.
+missing_rows = [x[:7] for x in missing]
+
+# Find the one row with the only empty seat.
+row = min(missing_rows, key = missing_rows.count)
+passp = missing[missing_rows.index(row)]
+
+# Print the seat's ID.
+id = readBPass('F', 'B', range(128), passp[:7]) * 8 + readBPass('L', 'R', range(8), passp[7:])
+print(id)
